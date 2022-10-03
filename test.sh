dfe75a5b02083c7558936fd4896668288db3d1d2
William Desportes <williamdes@wdes.fr>
2022-10-03 20:06:12 +0200

    Fixup configs

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ DID_FAIL=0
 
 checkUrl() {
     set +e
-    curl --fail -I "${1}"
+    curl -# --fail -I "${1}"
     if [ $? -gt 0 ]; then
         DID_FAIL=1
         echo "ERR: for URL ${1}"
@@ -23,6 +23,6 @@ checkUrl "http://${TEST_ADDR}/index.php"
 checkUrl "http://${TEST_ADDR}/robots.txt"
 
 if [ $DID_FAIL -gt 0 ]; then
-    echo "Showing logs"
-    docker logs test-bench
+    echo "Some URLs failed"
+    exit 1
 fi
